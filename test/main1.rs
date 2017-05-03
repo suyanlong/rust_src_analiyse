f2a213584171755cad316558dabf1bec5b4e2b1c
suyanlong <592938410@qq.com>
2017-05-03 15:04:21 +0800

    提交信息

--- test/main1.rs
+++ test/main1.rs
@@ -46,12 +46,8 @@ fn main() {
     //看到基本类型，怎么感觉也在标准库里面呢。
     println!("i32 max_values = {}", i32::max_value());
     let v1 = vec![1, 2, 3];
-
     let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
-
     assert_eq!(v2, [2, 3, 4]);
-
-
     println!("----end---");
 }
 
